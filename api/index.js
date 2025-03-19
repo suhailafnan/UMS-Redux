5b0f034dfefb049f5da58056ae82ed67397f201b
suhail afnan <suhailafnan00@gmail.com>
2025-03-19 21:21:52 +0530

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express' ;
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoute.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -11,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("listening on port number localhost:3000");
 });
@@ -18,3 +21,4 @@ app.listen(3000,()=>{
 
 // api route 
 app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes);
