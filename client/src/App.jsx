d28dda88f697777bfb6f6385f275ab199522ff14
suhail afnan <suhailafnan00@gmail.com>
2025-03-19 17:35:48 +0530

    initail page setup and route setup doen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; 
+import About from "./pages/About"; 
+import SignIn from "./pages/SignIn"; 
+import SignUp from "./pages/SignUp"; 
+import Profile from "./pages/Profile"; 
+
 
 const App = () => {
   return (
-    <div><h1 className='text-red-500'>ehdfsda</h1></div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/Sign=up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
